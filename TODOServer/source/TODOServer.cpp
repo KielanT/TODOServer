5514365391b38c5561172c9fa4ca5f1953bb67d7
Kielan <kielantodd@hotmail.com>
2024-09-26 14:36:57 +0100

    Create file on server

--- TODOServer/source/TODOServer.cpp
+++ TODOServer/source/TODOServer.cpp
@@ -8,30 +8,43 @@
 #include "crow.h"
 #include "crow/middlewares/cors.h"
 
-
-int GetCount()
+void CreateNewFile(const std::string& name = "")
 {
-    std::ifstream file("count.txt");
-    if (!file.is_open())
-        return 0;
+    std::string fileName = name;
 
-    std::string line;
-    getline(file, line);
-    file.close();
-    return std::stoi(line);
+    if (fileName.empty())
+    {
+        std::cout << "file name is empty" << std::endl;
+        const std::string baseName = "untitled";
+        fileName = "lists/" + baseName + ".txt";
+
+        int count = 1;
+
+        while (std::filesystem::exists(fileName))
+        {
+            fileName = "lists/" + baseName + std::to_string(count) + ".txt";
+            std::cout << fileName << std::endl;
+            count++;
+        }
+    }
+    else
+    {
+        fileName = "lists/" + fileName + ".txt";
+    }
+
+   
+    std::cout << "new file = " << fileName << std::endl;
+
+    std::ofstream file(fileName);
+    if (!file)
+    {
+        std::cerr << "Error: Could not create file " << fileName << std::endl;
+    }
+    else
+    {
+        file.close();
+    }
 }
-
-void AppendCount(const int& count)
-{
-    std::ofstream file("count.txt");
-    if (!file.is_open())
-        return;
-    
-    file << count;
-    file.close();
-}
-
-
 
 int main()
 {
@@ -47,143 +60,14 @@ int main()
         .methods("POST"_method, "GET"_method)
         .origin("*");// TODO for security update this
 
-    CROW_ROUTE(app, "/")([]()
+
+    CROW_ROUTE(app, "/newList").methods(crow::HTTPMethod::POST)([]()
         {
-            int count = GetCount();
-
-            std::ostringstream html;
-
-            html << "<!doctype html>"
-                << "<html lang=\"en\">"
-                << "<head>"
-                << "<meta charset=\"utf-8\" />"
-                << "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />"
-                << "<title>Hosted website</title>"
-                << "<script>"
-                << "function sendClick() {"
-                << "  fetch('/update', { method: 'POST' })"
-                << "    .then(response => {"
-                << "      if (!response.ok) {"
-                << "        throw new Error('Network response was not ok: ' + response.status);"
-                << "      }"
-                << "      return response.json();"
-                << "    })"
-                << "    .then(data => {"
-                << "      document.getElementById('count').innerText = 'Clicked ' + data.count + ' times!';"
-                << "    })"
-                << "    .catch(error => {"
-                << "      console.error('There was a problem with the fetch operation:', error);"
-                << "    });"
-                << "}"
-                << "</script>"
-                << "</head>"
-                << "<body>"
-                << "<p id='count'>Clicked " << count << " times!</p>"
-                << "<button onclick='sendClick()'>Click Me</button>"
-                << "</body>"
-                << "</html>";
-
-
-            return html.str();
-        });
-
-    CROW_ROUTE(app, "/update").methods(crow::HTTPMethod::POST)([&]()
-        {
-            int count = GetCount();
-            count++;
-            AppendCount(count);
-
+            CreateNewFile();
+            
             crow::json::wvalue response;
-            response["count"] = count;
-
-            const auto& str = std::to_string(GetCount());
-            for (auto u : users)
-            {
-                u->send_text(str);
-            }
-
             return crow::response{ response };
         });
-
-    CROW_ROUTE(app, "/count").methods(crow::HTTPMethod::GET)([]()
-        {
-            int count = GetCount();
-            crow::json::wvalue response;
-            response["count"] = count;
-
-            return crow::response{ response };
-        });
-
-    CROW_ROUTE(app, "/reset").methods(crow::HTTPMethod::POST)([&]()
-        {
-            AppendCount(0);
-            crow::json::wvalue response;
-            response["count"] = 0;
-
-            for (auto u : users)
-            {
-                u->send_text("0");
-            }
-
-            return crow::response{ response };
-        });
-
-    
-
-    CROW_WEBSOCKET_ROUTE(app, "/ws")
-        .onopen([&](crow::websocket::connection& conn)
-            {
-                std::cout << "***********************************************" << std::endl;
-                std::cout << "connection at: " << conn.get_remote_ip() << std::endl;
-                std::cout << "***********************************************" << std::endl;
-
-                users.insert(&conn);
-            })
-        .onclose([&](crow::websocket::connection& conn, const std::string& reason/*, uint16_t statusCode*/)
-            {
-                CROW_LOG_INFO << "websocket connection closed: " << reason;
-                std::cout << "***********************************************" << std::endl;
-                std::cout << "closed at: " << conn.get_remote_ip() << std::endl;
-                std::cout << "***********************************************" << std::endl;
-                users.erase(&conn);
-
-            })
-        .onmessage([&](crow::websocket::connection& conn, const std::string& data, bool is_binary)
-            {
-                std::cout << "***********************************************" << std::endl;
-                std::cout << "message from: " << conn.get_remote_ip() << std::endl;
-                std::cout << data << std::endl;
-                std::cout << "***********************************************" << std::endl;
-
-                auto jsonData = crow::json::load(data);
-                std::cout << jsonData << std::endl;
-
-                if (!jsonData.has("type")) return;
-                std::string messageType = jsonData["type"].s();
-
-                const auto& str = std::to_string(GetCount()); // TODO capture count
-
-                for (auto u : users)
-                {
-                    if (messageType == "update")
-                    {
-                        int count = GetCount();
-                        count++;
-                        AppendCount(count);
-
-                        std::cout << "send update" << std::endl;
-                        if (is_binary)
-                            u->send_binary(str);
-                        else
-                            u->send_text(str);
-                    }
-                    else
-                    {
-                        std::cout << "failed to send" << std::endl;
-                    }
-                }
-
-            });
 
 	app.port(5000).multithreaded().run();
 
